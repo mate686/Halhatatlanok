b98ee8c6e9ce9d08c38cfc6539cce9bf964e24a1
mate686 <mate.gamang@gmail.com>
2026-03-01 19:47:08 +0100

    Refactor Feladat6Model to remove duplicates
    Removed duplicate class definition and merged properties.

--- Halhatatlanok/ViewModels/Feladat6Model.cs
+++ Halhatatlanok/ViewModels/Feladat6Model.cs
@@ -1,6 +1,5 @@
 ﻿namespace Halhatatlanok.ViewModels
 {
-<<<<<<< HEAD
     
         public class Feladat6Model
         {
@@ -11,16 +10,5 @@
 
             public int Ev { get; set; }
         }
-    
-=======
-    public class Feladat6Model
-    {
 
-        public string Foglalkozas { get; set; }
-
-        public string Nev {  get; set; }
-
-        public int Ev { get; set; }
-    }
->>>>>>> 017e2ad6286c4a30b3e42f459bf70d3529153df4
 }
